bd02d56f9a318404eeab4dc6a57e2c6f2d12600c
Jonathan Easterman <jonathaneasterman@gmail.com>
2024-05-20 22:41:34 +0100

    Deploy mantle contracts

--- src/utils/Constants.sol
+++ src/utils/Constants.sol
@@ -16,13 +16,21 @@ uint256 constant BASE_SEPOLIA = 84532;
 uint256 constant FRAXTAL_MAINNET = 252;
 uint256 constant FRAXTAL_HOLESKY = 2522;
 
+uint256 constant MANTLE_SEPOLIA = 5003;
+uint256 constant MANTLE_MAINNET = 5000;
+
 address constant OP_STACK_L1_BLOCK_PREDEPLOY_ADDR =
     0x4200000000000000000000000000000000000015;
 
 address constant L1_BASE_BRIDGE = 0x3154Cf16ccdb4C6d922629664174b904d80F2C35;
+
 address constant L1_FRAXTAL_BRIDGE = 0x34C0bD5877A5Ee7099D0f5688D65F4bB9158BDE2;
 address constant L1_FRAXTAL_HOLESKY_BRIDGE =
     0x0BaafC217162f64930909aD9f2B27125121d6332;
+
+address constant L1_MANTLE_BRIDGE = 0xb4133552BA49dFb60DA6eb5cA0102d0f94ce071f;
+address constant L1_MANTLE_SEPOLIA_BRIDGE =
+    0xf26e9932106E6477a4Ae15dA0eDDCdB985065a1a;
 
 address constant PUDGEY_PENGUINS = 0xBd3531dA5CF5857e7CfAA92426877b022e612cf8;
 
@@ -45,29 +53,39 @@ function isOPStack() view returns (bool) {
     return (size > 0);
 }
 
+function isMantle() view returns (bool) {
+    return block.chainid == MANTLE_MAINNET || block.chainid == MANTLE_MAINNET;
+}
+
 function isLocal() view returns (bool) {
     return block.chainid == LOCAL || block.chainid == ANVIL;
 }
 
 function isTestnet() view returns (bool) {
-    uint256[4] memory testnets =
-        [ETH_SEPOLIA, ETH_HOLESKY, BASE_SEPOLIA, FRAXTAL_HOLESKY];
+    uint256[5] memory testnets = [
+        ETH_SEPOLIA,
+        ETH_HOLESKY,
+        BASE_SEPOLIA,
+        FRAXTAL_HOLESKY,
+        MANTLE_SEPOLIA
+    ];
     return chainMatches(testnets);
 }
 
 function isMainnet() view returns (bool) {
-    uint256[3] memory mainnets = [ETH_MAINNET, BASE_MAINNET, FRAXTAL_MAINNET];
+    uint256[4] memory mainnets =
+        [ETH_MAINNET, BASE_MAINNET, FRAXTAL_MAINNET, MANTLE_MAINNET];
     return chainMatches(mainnets);
 }
 
-function chainMatches(uint256[3] memory chains) view returns (bool) {
+function chainMatches(uint256[4] memory chains) view returns (bool) {
     for (uint256 i = 0; i < chains.length; i++) {
         if (chains[i] == block.chainid) return true;
     }
     return false;
 }
 
-function chainMatches(uint256[4] memory chains) view returns (bool) {
+function chainMatches(uint256[5] memory chains) view returns (bool) {
     for (uint256 i = 0; i < chains.length; i++) {
         if (chains[i] == block.chainid) return true;
     }
