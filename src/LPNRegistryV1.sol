e12ec85329b2d1a8f42b609d2ac3ed8dadd70406
Jonathan Easterman <jonathaneasterman@gmail.com>
2024-08-05 14:08:52 +0100

    initial draft of v1 contracts

---
+++ src/LPNRegistryV1.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {OwnableWhitelist} from "./utils/OwnableWhitelist.sol";
+import {Initializable} from
+    "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {RegistrationManager} from "./RegistrationManager.sol";
+import {QueryManager} from "./QueryManager.sol";
+
+/// @title LPNRegistryV1
+/// @notice A registry contract for managing LPN (Lagrange Proving Network) registrations and requests.
+contract LPNRegistryV1 is
+    QueryManager,
+    RegistrationManager,
+    OwnableWhitelist,
+    Initializable
+{
+    function initialize(address owner) external initializer {
+        OwnableWhitelist._initialize(owner);
+    }
+
+    /// @dev Only owner can register tables currently
+    function registerTable(
+        bytes32 hash,
+        address contractAddr,
+        uint96 chainId,
+        uint256 genesisBlock,
+        string calldata name,
+        string calldata schema
+    ) external onlyOwner {
+        // TODO: Implement payment model
+        _registerTable(hash, contractAddr, chainId, genesisBlock, name, schema);
+    }
+
+    /// @notice The relayer withdraws all fees accumulated
+    function withdrawFees() external onlyOwner returns (bool) {
+        return _withdrawFees();
+    }
+}
---
+++ src/interfaces/IRegistrationManager.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/// @title IRegistrationManager
+/// @notice
+interface IRegistrationManager {
+    event NewTableRegistration(
+        bytes32 indexed hash,
+        address indexed contractAddr,
+        uint256 chainId,
+        uint256 genesisBlock,
+        string name,
+        string schema
+    );
+
+    event NewQueryRegistration(
+        bytes32 indexed hash, bytes32 indexed tableHash, string indexed sql
+    );
+
+    function registerQuery(bytes32 hash, bytes32 tableHash, string calldata sql)
+        external;
+}
