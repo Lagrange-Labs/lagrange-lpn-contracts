0573b2918de32926015aee73f28239e803c84497
Ryan Hall <RyanRHall@users.noreply.github.com>
2024-11-12 14:52:27 +0700

    fix pragmas to 0.8.25

--- src/v1/RegistrationManager.sol
+++ src/v1/RegistrationManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.25;
 
 import {isEthereum, isOPStack, isMantle, isCDK} from "../utils/Constants.sol";
 import {IRegistrationManager} from "./interfaces/IRegistrationManager.sol";
