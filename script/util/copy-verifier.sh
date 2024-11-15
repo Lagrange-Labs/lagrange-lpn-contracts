1e0afa32d5cf77cdd053c783440300cb076ab8b2
nikkolasg <nikkolasg@gmail.com>
2024-11-15 01:16:11 +0100

    update dev

--- script/util/copy-verifier.sh
+++ script/util/copy-verifier.sh
@@ -7,42 +7,46 @@ VERIFIER_FILE=./src/v1/Groth16Verifier.sol
 VERIFIER_EXTENSIONS_FILE=./src/v1/Groth16VerifierExtensions.sol
 #VERIFIER_SOL_URL="https://pub-64a4eb6e897e425083647b3e0e8539a1.r2.dev/groth16_assets/verifier.sol"
 
-
 case "$1" in
-  dev)
-    VERIFIER_SOL_URL="https://pub-64a4eb6e897e425083647b3e0e8539a1.r2.dev"
-    ;;
-  test)
-    VERIFIER_SOL_URL="https://pub-fbb5db8dc9ee4e8da9daf13e07d27c24.r2.dev"
-    ;;
-  *)
-    echo "Usage: $0 {dev|test}"
-    exit 1
-    ;;
+dev)
+  VERIFIER_SOL_URL="https://pub-64a4eb6e897e425083647b3e0e8539a1.r2.dev"
+  ;;
+test)
+  VERIFIER_SOL_URL="https://pub-fbb5db8dc9ee4e8da9daf13e07d27c24.r2.dev"
+  ;;
+*)
+  echo "Usage: $0 {dev|test}"
+  exit 1
+  ;;
 esac
 
 echo "VERIFIER_SOL_URL: $VERIFIER_SOL_URL"
 
 VERIFIER_SOL_URL="$VERIFIER_SOL_URL/groth16_assets/verifier.sol"
+echo "VERIFIER_SOL_URL: $VERIFIER_SOL_URL"
 
-cd $GIT_REPO_PATH && \
-    git fetch origin $BRANCH && \
-    git checkout $BRANCH && \
-    git pull origin $BRANCH && \
-    cd -
+cd $GIT_REPO_PATH &&
+  git fetch origin $BRANCH &&
+  git checkout $BRANCH &&
+  git pull origin $BRANCH &&
+  cd -
 
 wget -O $VERIFIER_FILE $VERIFIER_SOL_URL
 cp "${GIT_REPO_PATH}/${CODE_DIR_PATH}/Groth16VerifierExtensions.sol" $VERIFIER_EXTENSIONS_FILE
+echo "Verifier extension copied from mp2"
 
 # Use as library instead of contract
-sed -i '' 's/contract Verifier/library Groth16Verifier/' $VERIFIER_FILE
+echo "111111111111111111"
+sed -i 's/contract Verifier/library Groth16Verifier/' $VERIFIER_FILE
+
+echo "2222222222222222222"
 
 # Use internal instead of public functions
-sed -i '' 's/public view/internal view/' $VERIFIER_FILE
+sed -i 's/public view/internal view/' $VERIFIER_FILE
 
 # Read proof argument from memory instead of calldata
-sed -i '' 's/calldata proof/memory proof/' $VERIFIER_FILE
-sed -i '' $'s/calldatacopy(f, proof, 0x100)/mstore(f, mload(add(proof, 0x00)))\
+sed -i 's/calldata proof/memory proof/' $VERIFIER_FILE
+sed -i $'s/calldatacopy(f, proof, 0x100)/mstore(f, mload(add(proof, 0x00)))\
     mstore(add(f, 0x20), mload(add(proof, 0x20)))\
     mstore(add(f, 0x40), mload(add(proof, 0x40)))\
     mstore(add(f, 0x60), mload(add(proof, 0x60)))\
@@ -52,52 +56,52 @@ sed -i '' $'s/calldatacopy(f, proof, 0x100)/mstore(f, mload(add(proof, 0x00)))\
     mstore(add(f, 0xe0), mload(add(proof, 0xe0)))/' $VERIFIER_FILE
 
 # Read input argument from memory instead of calldata
-sed -i '' 's/calldata input/memory input/' $VERIFIER_FILE
-sed -i '' 's/calldataload(input)/mload(input)/' $VERIFIER_FILE
-sed -i '' 's/calldataload(add(input, 32))/mload(add(input, 32))/' $VERIFIER_FILE
-sed -i '' 's/calldataload(add(input, 64))/mload(add(input, 64))/' $VERIFIER_FILE
+sed -i 's/calldata input/memory input/' $VERIFIER_FILE
+sed -i 's/calldataload(input)/mload(input)/' $VERIFIER_FILE
+sed -i 's/calldataload(add(input, 32))/mload(add(input, 32))/' $VERIFIER_FILE
+sed -i 's/calldataload(add(input, 64))/mload(add(input, 64))/' $VERIFIER_FILE
 
 # Import verifier library with renamed filename
-sed -i '' 's/import {Verifier} from ".\/verifier.sol";/import {Groth16Verifier} from ".\/Groth16Verifier.sol";\
+sed -i 's/import {Verifier} from ".\/verifier.sol";/import {Groth16Verifier} from ".\/Groth16Verifier.sol";\
    import {isCDK} from "..\/utils\/Constants.sol";/' $VERIFIER_EXTENSIONS_FILE
 
 # Use extensions as library instead of contract
-sed -i '' 's/contract Query is Verifier {/library Groth16VerifierExtensions {/' $VERIFIER_EXTENSIONS_FILE
-sed -i '' 's/CIRCUIT_DIGEST/Groth16Verifier.CIRCUIT_DIGEST/' $VERIFIER_EXTENSIONS_FILE
-sed -i '' 's/this.verifyProof/Groth16Verifier.verifyProof/' $VERIFIER_EXTENSIONS_FILE
+sed -i 's/contract Query is Verifier {/library Groth16VerifierExtensions {/' $VERIFIER_EXTENSIONS_FILE
+sed -i 's/CIRCUIT_DIGEST/Groth16Verifier.CIRCUIT_DIGEST/' $VERIFIER_EXTENSIONS_FILE
+sed -i 's/this.verifyProof/Groth16Verifier.verifyProof/' $VERIFIER_EXTENSIONS_FILE
 
 # Use internal instead of public functions
-sed -i '' 's/public view/internal view/' $VERIFIER_EXTENSIONS_FILE
+sed -i 's/public view/internal view/' $VERIFIER_EXTENSIONS_FILE
 
 # Patch `verifyQuery` function to be `view` instead of `pure`
-sed -i '' '/function verifyQuery.*/,+2 s/pure/view/' $VERIFIER_EXTENSIONS_FILE
+sed -i '/function verifyQuery.*/,+2 s/pure/view/' $VERIFIER_EXTENSIONS_FILE
 
 # Patch `verifyQuery` function to skip blockhash verification for polygon CDK chains
-sed -i '' 's/blockHash == query.blockHash/isCDK() || blockHash == query.blockHash/' $VERIFIER_EXTENSIONS_FILE
+sed -i 's/blockHash == query.blockHash/isCDK() || blockHash == query.blockHash/' $VERIFIER_EXTENSIONS_FILE
 
 forge fmt
 
 # verifyProof
-    # calldatacopy(f, proof, 0x100)
-    # ===>
-    # mstore(f, mload(add(proof, 0x00)))
-    # mstore(add(f, 0x20), mload(add(proof, 0x20)))
-    # mstore(add(f, 0x40), mload(add(proof, 0x40)))
-    # mstore(add(f, 0x60), mload(add(proof, 0x60)))
-    # mstore(add(f, 0x80), mload(add(proof, 0x80)))
-    # mstore(add(f, 0xa0), mload(add(proof, 0xa0)))
-    # mstore(add(f, 0xc0), mload(add(proof, 0xc0)))
-    # mstore(add(f, 0xe0), mload(add(proof, 0xe0)))
+# calldatacopy(f, proof, 0x100)
+# ===>
+# mstore(f, mload(add(proof, 0x00)))
+# mstore(add(f, 0x20), mload(add(proof, 0x20)))
+# mstore(add(f, 0x40), mload(add(proof, 0x40)))
+# mstore(add(f, 0x60), mload(add(proof, 0x60)))
+# mstore(add(f, 0x80), mload(add(proof, 0x80)))
+# mstore(add(f, 0xa0), mload(add(proof, 0xa0)))
+# mstore(add(f, 0xc0), mload(add(proof, 0xc0)))
+# mstore(add(f, 0xe0), mload(add(proof, 0xe0)))
 
 # publicInputMSM
-    # s := calldataload(input)
-    # ===>
-    # s := mload(input)
+# s := calldataload(input)
+# ===>
+# s := mload(input)
 
-    # s := calldataload(add(input, 32))
-    # ===>
-    # s := mload(add(input, 32))
+# s := calldataload(add(input, 32))
+# ===>
+# s := mload(add(input, 32))
 
-    # s := calldataload(add(input, 64))
-    # ===>
-    # s := mload(add(input, 64))
+# s := calldataload(add(input, 64))
+# ===>
+# s := mload(add(input, 64))
