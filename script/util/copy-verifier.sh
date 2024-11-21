56be99cb3845bef697a9860fc9aa17980f959718
Ryan Hall <RyanRHall@users.noreply.github.com>
2024-11-21 16:36:58 +0700

    update sed command in copy-verifier

--- script/util/copy-verifier.sh
+++ script/util/copy-verifier.sh
@@ -6,14 +6,15 @@ ENV=$1
 VERIFIER_FOLDER=./script/output/$1
 VERIFIER_FILE=$VERIFIER_FOLDER/Groth16Verifier.sol.ignore
 VERIFIER_EXTENSIONS_FILE=$VERIFIER_FOLDER/Groth16VerifierExtensions.sol.ignore
-#VERIFIER_SOL_URL="https://pub-64a4eb6e897e425083647b3e0e8539a1.r2.dev/groth16_assets/verifier.sol"
 
-
-case $(uname -s) in
-    Linux*)     SED="sed -i";;
-    Darwin*)    SED="sed -i ''";;
+# system agnostic sed
+sa_sed() {
+  case $(uname -s) in
+    Linux*)     sed -i "$@";;
+    Darwin*)    sed -i '' "$@";;
     *)          echo "Unknown OS; please adapt the sed selection process"; exit 1;;
-esac
+  esac
+}
 
 case "$ENV" in
   dev-0)
@@ -54,14 +55,14 @@ wget -O $VERIFIER_FILE $VERIFIER_SOL_URL
 cp "${GIT_REPO_PATH}/${CODE_DIR_PATH}/Groth16VerifierExtensions.sol" $VERIFIER_EXTENSIONS_FILE
 
 # Use as library instead of contract
-$SED 's/contract Verifier/library Groth16Verifier/' $VERIFIER_FILE
+sa_sed 's/contract Verifier/library Groth16Verifier/' $VERIFIER_FILE
 
 # Use internal instead of public functions
-$SED 's/public view/internal view/' $VERIFIER_FILE
+sa_sed 's/public view/internal view/' $VERIFIER_FILE
 
 # Read proof argument from memory instead of calldata
-$SED 's/calldata proof/memory proof/' $VERIFIER_FILE
-$SED $'s/calldatacopy(f, proof, 0x100)/mstore(f, mload(add(proof, 0x00)))\
+sa_sed 's/calldata proof/memory proof/' $VERIFIER_FILE
+sa_sed $'s/calldatacopy(f, proof, 0x100)/mstore(f, mload(add(proof, 0x00)))\
     mstore(add(f, 0x20), mload(add(proof, 0x20)))\
     mstore(add(f, 0x40), mload(add(proof, 0x40)))\
     mstore(add(f, 0x60), mload(add(proof, 0x60)))\
@@ -71,28 +72,28 @@ $SED $'s/calldatacopy(f, proof, 0x100)/mstore(f, mload(add(proof, 0x00)))\
     mstore(add(f, 0xe0), mload(add(proof, 0xe0)))/' $VERIFIER_FILE
 
 # Read input argument from memory instead of calldata
-$SED 's/calldata input/memory input/' $VERIFIER_FILE
-$SED 's/calldataload(input)/mload(input)/' $VERIFIER_FILE
-$SED 's/calldataload(add(input, 32))/mload(add(input, 32))/' $VERIFIER_FILE
-$SED 's/calldataload(add(input, 64))/mload(add(input, 64))/' $VERIFIER_FILE
+sa_sed 's/calldata input/memory input/' $VERIFIER_FILE
+sa_sed 's/calldataload(input)/mload(input)/' $VERIFIER_FILE
+sa_sed 's/calldataload(add(input, 32))/mload(add(input, 32))/' $VERIFIER_FILE
+sa_sed 's/calldataload(add(input, 64))/mload(add(input, 64))/' $VERIFIER_FILE
 
 # Import verifier library with renamed filename
-$SED 's/import {Verifier} from ".\/verifier.sol";/import {Groth16Verifier} from ".\/Groth16Verifier.sol";\
+sa_sed 's/import {Verifier} from ".\/verifier.sol";/import {Groth16Verifier} from ".\/Groth16Verifier.sol";\
    import {isCDK} from "..\/utils\/Constants.sol";/' $VERIFIER_EXTENSIONS_FILE
 
 # Use extensions as library instead of contract
-$SED 's/contract Query is Verifier {/library Groth16VerifierExtensions {/' $VERIFIER_EXTENSIONS_FILE
-$SED 's/CIRCUIT_DIGEST/Groth16Verifier.CIRCUIT_DIGEST/' $VERIFIER_EXTENSIONS_FILE
-$SED 's/this.verifyProof/Groth16Verifier.verifyProof/' $VERIFIER_EXTENSIONS_FILE
+sa_sed 's/contract Query is Verifier {/library Groth16VerifierExtensions {/' $VERIFIER_EXTENSIONS_FILE
+sa_sed 's/CIRCUIT_DIGEST/Groth16Verifier.CIRCUIT_DIGEST/' $VERIFIER_EXTENSIONS_FILE
+sa_sed 's/this.verifyProof/Groth16Verifier.verifyProof/' $VERIFIER_EXTENSIONS_FILE
 
 # Use internal instead of public functions
-$SED 's/public view/internal view/' $VERIFIER_EXTENSIONS_FILE
+sa_sed 's/public view/internal view/' $VERIFIER_EXTENSIONS_FILE
 
 # Patch `verifyQuery` function to be `view` instead of `pure`
-$SED '/function verifyQuery.*/,+2 s/pure/view/' $VERIFIER_EXTENSIONS_FILE
+sa_sed '/function verifyQuery.*/,+2 s/pure/view/' $VERIFIER_EXTENSIONS_FILE
 
 # Patch `verifyQuery` function to skip blockhash verification for polygon CDK chains
-$SED 's/blockHash == query.blockHash/isCDK() || blockHash == query.blockHash/' $VERIFIER_EXTENSIONS_FILE
+sa_sed 's/blockHash == query.blockHash/isCDK() || blockHash == query.blockHash/' $VERIFIER_EXTENSIONS_FILE
 
 forge fmt
 
