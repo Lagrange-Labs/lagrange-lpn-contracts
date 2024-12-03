d7b349609e6405ad225fbc0897369b55fbc4c07b
Ryan Hall <RyanRHall@users.noreply.github.com>
2024-12-03 11:44:53 -0700

    update comment

--- test/v1/BaseTest.t.sol
+++ test/v1/BaseTest.t.sol
@@ -41,8 +41,8 @@ abstract contract BaseTest is Test {
         }
     }
 
-    /// @dev this is a brute-force implementation of isOPStack from Constants.sol
-    /// we need this in tests because OP_STACK_L1_BLOCK_PREDEPLOY_ADDR doesn't exist on the test chain
+    /// @dev this is a brute-force implementation of isOPStack, and intentionally different from the one in Constants.sol.
+    /// We need this in tests because OP_STACK_L1_BLOCK_PREDEPLOY_ADDR doesn't exist on the test chain
     function _isOPStack(uint256 chainId) private view returns (bool) {
         uint256[6] memory OPChains = [
             BASE_MAINNET,
