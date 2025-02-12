f677ccd3260141abc2e24d2831e4b1d9fa451d1a
Ryan Hall <RyanRHall@users.noreply.github.com>
2025-02-12 14:49:03 -0300

    write LagrangeQueryRouter

--- test/v2/BaseTest.t.sol
+++ test/v2/BaseTest.t.sol
@@ -17,12 +17,47 @@ import {
 } from "../../src/utils/Constants.sol";
 
 abstract contract BaseTest is Test {
+    /// @notice The nonce for generating pseudo-random values
     uint256 private nonce;
 
     /// @notice Generates a random bytes32 value using an incrementing nonce
     /// @dev Hashes the nonce value and increments it to ensure different values on each call
     function randomBytes32() internal returns (bytes32) {
         return keccak256(abi.encodePacked(++nonce));
+    }
+
+    /// @notice Generates a random bytes array of a given length
+    function randomBytes(uint256 length) internal returns (bytes memory) {
+        bytes memory result = new bytes(length);
+        for (uint256 i = 0; i < length; i++) {
+            result[i] = randomBytes32()[0];
+        }
+        return result;
+    }
+
+    /// @notice Asserts that a value is present in a list
+    function assertContains(address[] memory list, address value)
+        internal
+        pure
+    {
+        for (uint256 i = 0; i < list.length; i++) {
+            if (list[i] == value) {
+                return;
+            }
+        }
+        revert("Address not found in list");
+    }
+
+    /// @notice Asserts that a value is *not* present in a list
+    function assertDoesNotContain(address[] memory list, address value)
+        internal
+        pure
+    {
+        for (uint256 i = 0; i < list.length; i++) {
+            if (list[i] == value) {
+                revert("Address found in list");
+            }
+        }
     }
 
     /// @notice Imitates a chain by setting the chainID and deploying arbitrary bytecode to certain predeploys
