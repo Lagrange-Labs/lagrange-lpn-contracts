e5f24c7e1cc8701c419705cfc72bf408365a2a6e
Ryan Hall <RyanRHall@users.noreply.github.com>
2025-03-19 13:52:46 -0300

    migrate tableHash to tableID

--- src/v2/DatabaseManager.sol
+++ src/v2/DatabaseManager.sol
@@ -24,27 +24,20 @@ contract DatabaseManager is
     bytes32 private constant OWNER_ROLE = keccak256("OWNER_ROLE");
 
     /// @notice Mapping to track registered tables
-    mapping(bytes32 tableHash => bool registered) private s_tables;
+    mapping(bytes32 tableID => bool registered) private s_tables;
 
     /// @notice Mapping to track registered queries, and the tables they are registered to
-    mapping(bytes32 queryHash => bytes32 tableHash) private s_queries;
+    mapping(bytes32 queryHash => bytes32 tableID) private s_queries;
 
     /// @notice Emitted when a new table is registered
-    event NewTableRegistration(
-        bytes32 indexed hash,
-        address indexed contractAddr,
-        uint256 chainId,
-        uint256 genesisBlock,
-        string name,
-        string schema
-    );
+    event NewTableRegistered(bytes32 indexed id);
 
     /// @notice Emitted when a table is deleted
-    event TableDeleted(bytes32 indexed hash);
+    event TableDeleted(bytes32 indexed id);
 
     /// @notice Emitted when a new query is registered.
-    event NewQueryRegistration(
-        bytes32 indexed hash, bytes32 indexed tableHash, string sql
+    event NewQueryRegistered(
+        bytes32 indexed hash, bytes32 indexed tableID, string sql
     );
 
     /// @notice Error thrown when attempting to register a table more than once
@@ -67,49 +60,35 @@ contract DatabaseManager is
     }
 
     /// @notice Registers a new table
-    /// @param hash The hash of the table, used as it's unique identifier
-    /// @param contractAddr The address of the contract that is registering the table
-    /// @param chainId The chain ID of the table
-    /// @param genesisBlock The genesis block of the table
-    /// @param name The name of the table
-    /// @param schema The schema of the table
-    function registerTable(
-        bytes32 hash,
-        address contractAddr,
-        uint96 chainId,
-        uint256 genesisBlock,
-        string calldata name,
-        string calldata schema
-    ) external onlyRole(OWNER_ROLE) {
-        if (s_tables[hash]) {
+    /// @param id The unique identifier for the table, generated off-chain
+    function registerTable(bytes32 id) external onlyRole(OWNER_ROLE) {
+        if (s_tables[id]) {
             revert TableAlreadyRegistered();
         }
-        s_tables[hash] = true;
-        emit NewTableRegistration(
-            hash, contractAddr, chainId, genesisBlock, name, schema
-        );
+        s_tables[id] = true;
+        emit NewTableRegistered(id);
     }
 
     /// @notice Deletes a registered table
-    /// @param hash The hash of the table to delete
-    function deleteTable(bytes32 hash) external onlyRole(OWNER_ROLE) {
-        delete s_tables[hash];
-        emit TableDeleted(hash);
+    /// @param id The id of the table to delete
+    function deleteTable(bytes32 id) external onlyRole(OWNER_ROLE) {
+        delete s_tables[id];
+        emit TableDeleted(id);
     }
 
     /// @notice Registers a new query
     /// @param hash The hash of the query, used as it's unique identifier
-    /// @param tableHash The hash of the table that the query is registered to
+    /// @param tableID The hash of the table that the query is registered to
     /// @param sql The raw SQL of the query
     /// @dev The hash is deterministed from the SQL; if it doesn't match then the query is conidered invalid
-    function registerQuery(bytes32 hash, bytes32 tableHash, string calldata sql)
+    function registerQuery(bytes32 hash, bytes32 tableID, string calldata sql)
         external
     {
         if (s_queries[hash] != bytes32(0)) {
             revert QueryAlreadyRegistered();
         }
-        s_queries[hash] = tableHash;
-        emit NewQueryRegistration(hash, tableHash, sql);
+        s_queries[hash] = tableID;
+        emit NewQueryRegistered(hash, tableID, sql);
     }
 
     /// @inheritdoc IDatabaseManager
@@ -118,13 +97,13 @@ contract DatabaseManager is
     }
 
     /// @notice Checks if a table is registered
-    /// @param hash The hash of the table
+    /// @param id The id of the table
     /// @return bool Trueif the table is registered, false otherwise
-    function isTableActive(bytes32 hash) external view returns (bool) {
-        return s_tables[hash];
+    function isTableActive(bytes32 id) external view returns (bool) {
+        return s_tables[id];
     }
 
-    /// @notice Gets the table hash associated with a query
+    /// @notice Gets the tableID associated with a query
     /// @param queryHash The hash of the query
     /// @return bytes32 The id of the table the query is registered to
     function getTableForQuery(bytes32 queryHash)
